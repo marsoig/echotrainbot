90413c4aff37c8a650fa02553d072cce13eac5a0
marsoig <50marsoig@users.noreply.github.com>
2020-09-12 10:41:32 +0300

    New functions structure

---
+++ app/ExperimentalMain.hs
@@ -0,0 +1,214 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE MultiWayIf #-}
+
+import Lib
+
+import           GHC.Generics
+import           Network.HTTP.Conduit           (simpleHttp)
+import           Control.Lens
+import           Data.Aeson
+import           Data.Aeson.Lens
+import           Data.ByteString.Lazy.Internal  as BSL
+import           Control.Monad.Trans.Maybe
+import           Data.Maybe                     (fromMaybe)
+--import qualified Data.ByteString.Char8         as BS
+--import         Data.Text                      ( Text )
+import           Data.Text.Lazy                as T (Text, concat, pack, unpack)
+--import qualified Data.Text.Lazy.IO             as T
+--import qualified Data.Text.Lazy.Encoding       as T
+import           Data.Maybe                    (fromJust)
+
+
+data User = User {  id                          :: Integer
+                  , is_bot                      :: Bool
+                  , first_name                  :: Text
+                  , last_name                   :: Maybe Text
+                  , username                    :: Maybe Text
+                  , language_code               :: Maybe Text
+                  , can_join_groups             :: Maybe Text
+                  , can_read_all_group_messages :: Maybe Text
+                  , supports_inline_queries     :: Maybe Bool  } deriving (Show, Eq, Generic)
+
+instance FromJSON User
+
+data Chat = Chat {  chat_id         :: Integer} deriving (Show, Eq, Generic)
+
+instance FromJSON Chat where
+  parseJSON = withObject "chat" $ \o -> do
+    chat_id                      <- o.: "id"
+    return Chat {..}
+
+data Message = Message {  message_id :: Integer
+                        , from       :: Maybe User
+                        , date       :: Integer
+                        , chat       :: Chat
+                        , text       :: Maybe String
+                        , sticker    :: Maybe Sticker
+                        , animation  :: Maybe Animation
+                        , audio      :: Maybe Audio
+                        , document   :: Maybe Document
+                        , video      :: Maybe Video
+                        , video_note :: Maybe VideoNote
+                        , voice      :: Maybe Voice
+                        , contact    :: Maybe Contact
+                        , caption    :: Maybe String
+                        , photo      :: Maybe [PhotoSize]} deriving (Show, Eq, Generic)
+
+instance FromJSON Message
+
+data Results = Results {  update_id :: Integer
+                        , message   :: Message} deriving (Show, Eq, Generic)
+
+instance FromJSON Results
+data Updates = Updates { ok      :: Bool
+                       , results :: [Results]} deriving (Show, Eq, Generic)
+
+instance FromJSON Updates
+
+data PhotoSize = PhotoSize {  file_id        :: String
+                            , file_unique_id :: String
+                            , width          :: Integer
+                            , height         :: Integer
+                            , file_size      :: Maybe Integer} deriving (Show, Eq, Generic)
+
+instance FromJSON PhotoSize
+
+data Sticker = Sticker {  file_id        :: Text
+                        , file_unique_id :: Text
+                        , width          :: Integer
+                        , height         :: Integer
+                        , is_animated    :: Bool
+                        , emoji          :: Maybe String
+                        , set_name       :: Maybe String
+                        , file_size      :: Maybe Integer} deriving (Show, Eq, Generic)
+
+instance FromJSON Sticker
+
+data Animation = Animation {  file_id        :: String
+                            , file_unique_id :: String
+                            , width          :: Integer
+                            , height         :: Integer
+                            , duration       :: Integer
+                            , thumb          :: Maybe PhotoSize
+                            , file_name      :: Maybe String
+                            , mime_type      :: Maybe String
+                            , file_size      :: Maybe Integer} deriving (Show, Eq, Generic)
+
+instance FromJSON Animation
+
+data Audio = Audio {  file_id        :: String
+                    , file_unique_id :: String
+                    , duration       :: Integer
+                    , performer            :: Maybe String
+                    , title                :: Maybe String
+                    , mime_type            :: Maybe String
+                    , file_size            :: Maybe Integer
+                    , thumb          :: Maybe PhotoSize} deriving (Show, Eq, Generic)
+
+instance FromJSON Audio
+
+data Document = Document {  file_id        :: String
+                          , file_unique_id :: String
+                          , thumb          :: Maybe PhotoSize
+                          , file_name               :: Maybe String
+                          , mime_type               :: Maybe String
+                          , file_size               :: Maybe Integer} deriving (Show, Eq, Generic)
+
+instance FromJSON Document
+
+data Video = Video {  file_id        :: String
+                    , file_unique_id :: String
+                    , width          :: Integer
+                    , height         :: Integer
+                    , duration       :: Integer
+                    , thumb          :: Maybe PhotoSize
+                    , mime_type      :: Maybe String
+                    , file_size      :: Maybe Integer} deriving (Show, Eq, Generic)
+
+instance FromJSON Video
+
+data VideoNote = VideoNote {  file_id        :: String
+                            , file_unique_id :: String
+                            , length         :: Integer
+                            , duration       :: Integer
+                            , thumb          :: Maybe PhotoSize
+                            , file_size                 :: Maybe Integer} deriving (Show, Eq, Generic)
+
+instance FromJSON VideoNote
+
+data Voice = Voice {  file_id        :: String
+                    , file_unique_id :: String
+                    , duration       :: Integer
+                    , mime_type      :: Maybe String
+                    , file_size      :: Maybe Integer} deriving (Show, Eq, Generic)
+
+instance FromJSON Voice
+
+data Contact = Contact {  phone_number :: String
+                        , first_name   :: String
+                        , last_name    :: Maybe String
+                        , user_id      :: Maybe Integer
+                        , vcard        :: Maybe String} deriving (Show, Eq, Generic)
+
+instance FromJSON Contact
+
+
+data MessageType = MText | MSticker | MAnimation | MAudio | MDocument | MVideo | MVideoNote | MVoice | MContact | MPhoto | MError
+
+checkMessageType :: Message -> MessageType
+checkMessageType msg = if | (text $ msg) /= Nothing -> MText
+                          | (sticker $ msg) /= Nothing -> MSticker
+                          | (animation $ msg) /= Nothing -> MAnimation
+                          | (audio $ msg) /= Nothing -> MAudio
+                          | (document $ msg) /= Nothing -> MDocument
+                          | (video $ msg) /= Nothing -> MVideo
+                          | (video_note $ msg) /= Nothing -> MVideoNote
+                          | (voice $ msg) /= Nothing -> MVoice
+                          | (contact $ msg) /= Nothing -> MContact
+                          | (photo $ msg) /= Nothing -> MPhoto
+                          | otherwise -> MError
+
+urlToken = "https://api.telegram.org/bot1293826122:AAHMwYErxB-irpptkb7tvz8oP8ehHEEzRh8"
+
+decodeToUpdates :: ByteString -> Updates
+decodeToUpdates x = case decode x :: Maybe Updates of
+                      Just u -> u
+                      Nothing -> Updates { ok = False, results = []}
+
+processUpdates :: Updates -> 
+
+addChat :: Message -> String
+addChat msg = "?chat_id=" ++ (show (chat_id $ chat $ msg))
+
+data SendingSet = SendingSet {command :: String, parameters :: String }
+
+addMessageParameters :: Message -> SendingSet
+addMessageParameters msg = case checkMessageType msg of
+                                | MText -> SendingSet {command = "/sendMessage", parameters = ("&text=" ++ (fromJust $ text $ msg))}
+                                | MSticker -> SendingSet {command = "/sendSticker", parameters = ("&sticker=" ++ (file_id $ fromJust $ sticker $ msg))}
+                                | MAnimation -> SendingSet {command = "/sendAnimation", parameters = ("&animation=" ++ (file_id $ fromJust $ animation $ msg))}
+                                | MAudio -> SendingSet {command = "/sendAudio", parameters = ("&audio=" ++ (file_id $ fromJust $ audio $ msg))}
+                                | MDocument -> SendingSet {command = "/sendDocument", parameters = ("&document=" ++ (file_id $ fromJust $ document $ msg))}
+                                | MVideo -> SendingSet {command = "/sendVideo", parameters = ("&video=" ++ (file_id $ fromJust $ video $ msg))}
+                                | MVideoNote -> SendingSet {command = "/sendVideoNote", parameters = ("&video_note=" ++ (file_id $ fromJust $ video_note $ msg))}
+                                | MVoice -> SendingSet {command = "/sendVoice", parameters = ("&voice=" ++ (file_id $ fromJust $ voice $ msg))}
+                                | MContact -> SendingSet {command = "/sendContact", parameters = ("&phone_number=" ++ (phone_number $ fromJust $ contact $ msg) ++ "&first_name=" ++ (first_name $ fromJust $ contact $ msg))}
+                                | MPhoto -> SendingSet {command = "/sendPhoto", parameters = ("&photo=" ++ (file_id $ head $ fromJust $ photo $ msg))}
+                                | MError -> SendingSet {command = "/sendMessage", parameters = ("&text=Error with message type")}
+
+makeURLRequest :: Message -> SendingSet -> String
+makeURLRequest msg sendset = urlToken ++ (command $ sendset) ++ "?chat_id=" ++ (show $ chat_id $ chat $ msg) ++ (parameters $ sendset) ++ cap
+                              where cap = case (caption $ msg) of
+                                           | Nothing -> ""
+                                           | Just x -> "&caption=" ++ x
+
+
+getUpdates :: String -> IO (Updates)
+getUpdates adr = do
+    download <- simpleHttp adr
+    updates <- decodeToUpdates download
+
+    return updates-}
--- app/Main.hs
+++ app/Main.hs
@@ -23,7 +23,7 @@ import           Data.Maybe                    (fromJust)
 
 data User = User {  userId                      :: Integer
                   , is_bot                      :: Bool
-                  , first_name                  :: Text
+                  , user_first_name                  :: Text
                   , last_name                   :: Maybe Text
                   , username                    :: Maybe Text
                   , language_code               :: Maybe Text
@@ -35,7 +35,7 @@ instance FromJSON User where
  parseJSON = withObject "user" $ \o -> do
    userId                      <- o.: "id"
    is_bot                      <- o.: "is_bot"
-   first_name                  <- o.: "first_name"
+   user_first_name             <- o.: "first_name"
    last_name                   <- o.:? "last_name"
    username                    <- o.:? "username"
    language_code               <- o.:? "language_code"
@@ -67,7 +67,15 @@ data Message = Message {  message_id :: Integer
                         , chat       :: Chat
                         , text       :: Maybe Text
                         , sticker    :: Maybe Sticker
-                        , animation  :: Maybe Animation} deriving (Show, Eq, Generic)
+                        , animation  :: Maybe Animation
+                        , audio      :: Maybe Audio
+                        , document   :: Maybe Document
+                        , video      :: Maybe Video
+                        , video_note :: Maybe VideoNote
+                        , voice      :: Maybe Voice
+                        , contact    :: Maybe Contact
+                        , caption    :: Maybe Text
+                        , photo      :: Maybe Photo} deriving (Show, Eq, Generic)
 
 instance FromJSON Message where
  parseJSON = withObject "message" $ \o -> do
@@ -78,6 +86,14 @@ instance FromJSON Message where
    text                       <- o.:? "text"
    sticker                    <- o.:? "sticker"
    animation                  <- o.:? "animation"
+   audio                      <- o.:? "audio"
+   document                   <- o.:? "document"
+   video                      <- o.:? "video"
+   video_note                 <- o.:? "video_note"
+   voice                      <- o.:? "voice"
+   contact                    <- o.:? "contact"
+   caption                    <- o.:? "caption"
+   photo                      <- o.:? "photo"
    return Message {..}
 
 data Results = Results {  update_id :: Integer
@@ -141,59 +157,124 @@ instance FromJSON Animation where
   parseJSON = withObject "animation" $ \o -> do
     animation_file_id        <- o.: "file_id"
     animation_file_unique_id <- o.: "file_unique_id"
-    animation_width <- o.: "width"
-    animation_height <- o.: "height"
-    animation_duration <- o.: "duration"
-    animation_thumb <- o.:? "thumb"
-    file_name <- o.:? "file_name"
-    mime_type <- o.:? "mime_type"
-    file_size <- o.:? "file_size"
+    animation_width          <- o.: "width"
+    animation_height         <- o.: "height"
+    animation_duration       <- o.: "duration"
+    animation_thumb          <- o.:? "thumb"
+    file_name                <- o.:? "file_name"
+    mime_type                <- o.:? "mime_type"
+    file_size                <- o.:? "file_size"
     return Animation {..}
 
-data Audio = Audio {  file_id        :: String
-                    , file_unique_id :: String
-                    , duration       :: Integer
-                    , performer      :: Maybe String
-                    , title          :: Maybe String
-                    , mime_type      :: Maybe String
-                    , file_size      :: Maybe Integer
-                    , thumb          :: Maybe PhotoSize} deriving (Show, Eq, Generic)
+data Audio = Audio {  audio_file_id        :: String
+                    , audio_file_unique_id :: String
+                    , audio_duration       :: Integer
+                    , performer            :: Maybe String
+                    , title                :: Maybe String
+                    , mime_type            :: Maybe String
+                    , file_size            :: Maybe Integer
+                    , audio_thumb          :: Maybe PhotoSize} deriving (Show, Eq, Generic)
 
-data Document = Document {  file_id        :: String
-                          , file_unique_id :: String
-                          , thumb          :: Maybe PhotoSize
-                          , file_name      :: Maybe String
-                          , mime_type      :: Maybe String
-                          , file_size      :: Maybe Integer} deriving (Show, Eq, Generic)
+instance FromJSON Audio where
+  parseJSON = withObject "audio" $ \o -> do
+    audio_file_id        <- o.: "file_id"
+    audio_file_unique_id <- o.: "file_unique_id"
+    audio_duration       <- o.: "duration"
+    audio_thumb          <- o.:? "thumb"
+    performer            <- o.:? "performer"
+    title                <- o.:? "title"
+    mime_type            <- o.:? "mime_type"
+    file_size            <- o.:? "file_size"
+    return Audio {..}
 
-data Video = Video {  file_id        :: String
-                    , file_unique_id :: String
-                    , width          :: Integer
-                    , height         :: Integer
-                    , duration       :: Integer
-                    , thumb          :: Maybe PhotoSize
+data Document = Document {  document_file_id        :: String
+                          , document_file_unique_id :: String
+                          , document_thumb          :: Maybe PhotoSize
+                          , file_name               :: Maybe String
+                          , mime_type               :: Maybe String
+                          , file_size               :: Maybe Integer} deriving (Show, Eq, Generic)
+
+instance FromJSON Document where
+  parseJSON = withObject "document" $ \o -> do
+    document_file_id        <- o.: "file_id"
+    document_file_unique_id <- o.: "file_unique_id"
+    document_thumb          <- o.:? "thumb"
+    file_name               <- o.:? "file_name"
+    mime_type               <- o.:? "mime_type"
+    file_size               <- o.:? "file_size"
+    return Document {..}
+
+data Video = Video {  video_file_id        :: String
+                    , video_file_unique_id :: String
+                    , video_width          :: Integer
+                    , video_height         :: Integer
+                    , video_duration       :: Integer
+                    , video_thumb          :: Maybe PhotoSize
                     , mime_type      :: Maybe String
                     , file_size      :: Maybe Integer} deriving (Show, Eq, Generic)
 
-data VideoNote = VideoNote {  file_id        :: String
-                            , file_unique_id :: String
-                            , length         :: Integer
-                            , duration       :: Integer
-                            , thumb          :: Maybe PhotoSize
-                            , file_size      :: Maybe Integer} deriving (Show, Eq, Generic)
+instance FromJSON Video where
+  parseJSON = withObject "video" $ \o -> do
+    video_file_id        <- o.: "file_id"
+    video_file_unique_id <- o.: "file_unique_id"
+    video_width          <- o.: "width"
+    video_height         <- o.: "height"
+    video_duration       <- o.: "duration"
+    video_thumb          <- o.:? "thumb"
+    mime_type            <- o.:? "mime_type"
+    file_size            <- o.:? "file_size"
+    return Video {..}
 
-data Voice = Voice {  file_id        :: String
-                    , file_unique_id :: String
-                    , duration       :: Integer
+data VideoNote = VideoNote {  video_note_file_id        :: String
+                            , video_note_file_unique_id :: String
+                            , video_note_length         :: Integer
+                            , video_note_duration       :: Integer
+                            , video_note_thumb          :: Maybe PhotoSize
+                            , file_size                 :: Maybe Integer} deriving (Show, Eq, Generic)
+
+instance FromJSON VideoNote where
+  parseJSON = withObject "videonote" $ \o -> do
+    video_note_file_id        <- o.: "file_id"
+    video_note_file_unique_id <- o.: "file_unique_id"
+    video_note_duration       <- o.: "duration"
+    video_note_length         <- o.: "length"
+    video_note_thumb          <- o.:? "thumb"
+    file_size                 <- o.:? "file_size"
+    return VideoNote {..}
+
+data Voice = Voice {  voice_file_id        :: String
+                    , voice_file_unique_id :: String
+                    , voice_duration       :: Integer
                     , mime_type      :: Maybe String
                     , file_size      :: Maybe Integer} deriving (Show, Eq, Generic)
+
+instance FromJSON Voice where
+  parseJSON = withObject "voice" $ \o -> do
+    voice_file_id        <- o.: "file_id"
+    voice_file_unique_id <- o.: "file_unique_id"
+    voice_duration       <- o.: "duration"
+    mime_type               <- o.:? "mime_type"
+    file_size               <- o.:? "file_size"
+    return Voice {..}
 
 data Contact = Contact {  phone_number :: String
-                        , first_name   :: String
+                        , contact_first_name   :: String
                         , last_name    :: Maybe String
                         , user_id      :: Maybe Integer
                         , vcard        :: Maybe String} deriving (Show, Eq, Generic)
 
+instance FromJSON Contact where
+  parseJSON = withObject "contact" $ \o -> do
+    phone_number        <- o.: "phone_number"
+    contact_first_name  <- o.: "contact_first_name"
+    last_name           <- o.:? "last_name"
+    user_id             <- o.:? "user_id"
+    vcard               <- o.:? "vcard "
+    return Contact {..}
+
+data Photo = Photo {photo_list :: [PhotoSize]} deriving (Show, Eq, Generic)
+
+instance FromJSON Photo
 
 
 getUpdates :: String -> IO (Updates)
@@ -203,28 +284,75 @@ getUpdates adr = do
                    Just x  -> return x
                    Nothing -> return $ Updates { ok = False, results = []}
 
-makeTextAnswer :: Message -> IO (Text)
-makeTextAnswer userMessage = do
+sendText :: Message -> IO (Text)
+sendText userMessage = do
     messageText     <- pure (fromMaybe (pack []) (text $ userMessage)) :: IO Text
     messageChatId   <- pure (chatId.chat $ userMessage) :: IO Integer
     sendingQuery    <- pure (T.concat [pack "https://api.telegram.org/bot1293826122:AAHMwYErxB-irpptkb7tvz8oP8ehHEEzRh8/sendMessage?chat_id=", pack $ show messageChatId, pack "&text=", messageText]) :: IO Text
-    check           <- simpleHttp (unpack sendingQuery)
     return sendingQuery
 
-makeStickerAnswer :: Message -> IO (Text)
-makeStickerAnswer userMessage = do
+sendSticker :: Message -> IO (Text)
+sendSticker userMessage = do
     sticker_id      <- pure (sticker_file_id $ fromJust (sticker $ userMessage)) :: IO Text
     messageChatId   <- pure (chatId.chat $ userMessage) :: IO Integer
     sendingQuery    <- pure (T.concat [pack "https://api.telegram.org/bot1293826122:AAHMwYErxB-irpptkb7tvz8oP8ehHEEzRh8/sendSticker?chat_id=", pack $ show messageChatId, pack "&sticker=", sticker_id]) :: IO Text
-    check           <- simpleHttp (unpack sendingQuery)
     return sendingQuery
 
-makeAnimationAnswer :: Message -> IO (Text)
-makeAnimationAnswer userMessage = do
+sendAnimation :: Message -> IO (Text)
+sendAnimation userMessage = do
     messageChatId   <- pure (chatId.chat $ userMessage) :: IO Integer
     animation_id    <- pure (animation_file_id $ fromJust (animation $ userMessage)) :: IO String
     sendingQuery    <- pure (T.concat [pack "https://api.telegram.org/bot1293826122:AAHMwYErxB-irpptkb7tvz8oP8ehHEEzRh8/sendAnimation?chat_id=", pack (show messageChatId), pack "&animation=", pack animation_id]) :: IO Text
-    check           <- simpleHttp (unpack sendingQuery)
+    return sendingQuery
+
+sendAudio :: Message -> IO (Text)
+sendAudio userMessage = do
+    messageChatId   <- pure (chatId.chat $ userMessage) :: IO Integer
+    audio_id        <- pure (audio_file_id $ fromJust (audio $ userMessage)) :: IO String
+    sendingQuery    <- pure (T.concat [pack "https://api.telegram.org/bot1293826122:AAHMwYErxB-irpptkb7tvz8oP8ehHEEzRh8/sendAudio?chat_id=", pack (show messageChatId), pack "&audio=", pack audio_id]) :: IO Text
+    return sendingQuery
+
+sendDocument :: Message -> IO (Text)
+sendDocument userMessage = do
+    messageChatId   <- pure (chatId.chat $ userMessage) :: IO Integer
+    document_id        <- pure (document_file_id $ fromJust (document $ userMessage)) :: IO String
+    sendingQuery    <- pure (T.concat [pack "https://api.telegram.org/bot1293826122:AAHMwYErxB-irpptkb7tvz8oP8ehHEEzRh8/sendDocument?chat_id=", pack (show messageChatId), pack "&document=", pack document_id]) :: IO Text
+    return sendingQuery
+
+sendVideo :: Message -> IO (Text)
+sendVideo userMessage = do
+    messageChatId   <- pure (chatId.chat $ userMessage) :: IO Integer
+    video_id        <- pure (video_file_id $ fromJust (video $ userMessage)) :: IO String
+    sendingQuery    <- pure (T.concat [pack "https://api.telegram.org/bot1293826122:AAHMwYErxB-irpptkb7tvz8oP8ehHEEzRh8/sendVideo?chat_id=", pack (show messageChatId), pack "&video=", pack video_id]) :: IO Text
+    return sendingQuery
+
+sendVideoNote :: Message -> IO (Text)
+sendVideoNote userMessage = do
+    messageChatId   <- pure (chatId.chat $ userMessage) :: IO Integer
+    video_note_id   <- pure (video_note_file_id $ fromJust (video_note $ userMessage)) :: IO String
+    sendingQuery    <- pure (T.concat [pack "https://api.telegram.org/bot1293826122:AAHMwYErxB-irpptkb7tvz8oP8ehHEEzRh8/sendVideoNote?chat_id=", pack (show messageChatId), pack "&video_note=", pack video_note_id]) :: IO Text
+    return sendingQuery
+
+sendVoice :: Message -> IO (Text)
+sendVoice userMessage = do
+    messageChatId   <- pure (chatId.chat $ userMessage) :: IO Integer
+    voice_id        <- pure (voice_file_id $ fromJust (voice $ userMessage)) :: IO String
+    sendingQuery    <- pure (T.concat [pack "https://api.telegram.org/bot1293826122:AAHMwYErxB-irpptkb7tvz8oP8ehHEEzRh8/sendVoice?chat_id=", pack (show messageChatId), pack "&voice=", pack voice_id]) :: IO Text
+    return sendingQuery
+
+sendPhoto :: Message -> IO (Text)
+sendPhoto userMessage = do
+    messageChatId   <- pure (chatId.chat $ userMessage) :: IO Integer
+    photo_id        <- pure ( file_id $ head $ photo_list $ fromJust (photo $ userMessage)) :: IO String
+    sendingQuery    <- pure (T.concat [pack "https://api.telegram.org/bot1293826122:AAHMwYErxB-irpptkb7tvz8oP8ehHEEzRh8/sendPhoto?chat_id=", pack (show messageChatId), pack "&photo=", pack photo_id]) :: IO Text
+    return sendingQuery
+
+sendContact :: Message -> IO (Text)
+sendContact userMessage = do
+    messageChatId   <- pure (chatId.chat $ userMessage) :: IO Integer
+    phone_number    <- pure (phone_number $ fromJust (contact $ userMessage)) :: IO String
+    first_name      <- pure (contact_first_name $ fromJust (contact $ userMessage)) :: IO String
+    sendingQuery    <- pure (T.concat [pack "https://api.telegram.org/bot1293826122:AAHMwYErxB-irpptkb7tvz8oP8ehHEEzRh8/sendVoice?chat_id=", pack (show messageChatId), pack "&phone_number=", pack phone_number, pack "&first_name=", pack first_name]) :: IO Text
     return sendingQuery
 
 waitForUpdates :: String -> IO (Updates)
@@ -240,7 +368,18 @@ mainP updateId = do
     receivedUpdates <- waitForUpdates address
     lastMessage <- pure (message.last.results $ receivedUpdates) :: IO Message
     lastUpdateId <- pure (update_id.last.results $ receivedUpdates) :: IO Integer
-    if (text $ lastMessage) /= Nothing then makeTextAnswer lastMessage
-      else  if (sticker $ lastMessage) /= Nothing then makeStickerAnswer lastMessage
-        else if (animation $ lastMessage) /= Nothing then makeAnimationAnswer lastMessage else return ("Success")
+    sendingQuery <-
+      if (text $ lastMessage) /= Nothing then sendText lastMessage
+       else  if (sticker $ lastMessage) /= Nothing then sendSticker lastMessage
+        else if (animation $ lastMessage) /= Nothing then sendAnimation lastMessage
+          else  if (audio $ lastMessage) /= Nothing then sendAudio lastMessage
+            else if (document $ lastMessage) /= Nothing then sendDocument lastMessage
+              else  if (video $ lastMessage) /= Nothing then sendVideo lastMessage
+                else if (video_note $ lastMessage) /= Nothing then sendVideoNote lastMessage
+                  else  if (voice $ lastMessage) /= Nothing then sendVoice lastMessage
+                    else if (contact $ lastMessage) /= Nothing then sendContact lastMessage
+                      else if (photo $ lastMessage) /= Nothing then sendPhoto lastMessage
+                        else return ("Success")
+    capSendingQuery <- (\x -> pure x :: IO (String)) (if (caption $ lastMessage) /= Nothing then (unpack sendingQuery) ++ "&caption=" ++ (unpack (fromJust $ caption $ lastMessage)) else (unpack sendingQuery))
+    sending <- simpleHttp capSendingQuery
     mainP (lastUpdateId+1)
